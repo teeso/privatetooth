58cf597945178786db5b8b3ae0c1edff0b8d9621
Vitor Pamplona <vitor@vitorpamplona.com>
2020-03-21 20:10:33 -0400

    Generating new UUIDs every hour. Making myself discoverable every 3 minutes.

--- app/services/BroadcastingService.js
+++ app/services/BroadcastingService.js
@@ -7,12 +7,17 @@ import PushNotificationIOS from "@react-native-community/push-notification-ios";
 import PushNotification from "react-native-push-notification";
 import { BleManager } from 'react-native-ble-plx';
 
+import BackgroundTimer from 'react-native-background-timer';
+import UUIDGenerator from 'react-native-uuid-generator';
+
 var instanceCount = 0;
 var lastPointCount = 0;
 var broadcastingInterval = 60000 * 5;  // Time (in milliseconds) between broadcasting information polls.  E.g. 60000*5 = 5 minutes
 // DEBUG: Reduce Time intervall for faster debugging
 // var broadcastingInterval = 5000;
 var bleManager = new BleManager();
+var currentUUID = "";
+
 
 function saveContact(contact) {
     // Persist this contact data in our local storage of time/lat/lon values
@@ -48,9 +53,55 @@ function saveContact(contact) {
         });
 }
 
+function saveMyUUID(me) {
+    // Persist this contact data in our local storage of time/lat/lon values
+
+    GetStoreData('MY_UUIDs')
+        .then(myUUIDString => {
+            var myUUIDArray;
+            if (myUUIDArrayString !== null) {
+                myUUIDArray = JSON.parse(myUUIDArrayString);
+            } else {
+                myUUIDArray = [];
+            }
+
+            // Always work in UTC, not the local time in the contactData
+            var nowUTC = new Date().toISOString();
+            var unixtimeUTC = Date.parse(nowUTC);
+            var unixtimeUTC_28daysAgo = unixtimeUTC - (60 * 60 * 24 * 1000 * 28);
+
+            // Save the contact using the current lat-lon and the
+            // calculated UTC time (maybe a few milliseconds off from
+            // when the GPS data was collected, but that's unimportant
+            // for what we are doing.)
+            lastPointCount = myUUID.length;
+            console.log('[GPS] Saving myUUID:', lastPointCount);
+            var lat_lon_time = {
+                "uuid": me["uuid"],
+                "time": unixtimeUTC
+            };
+            myUUID.push(lat_lon_time);
+
+            SetStoreData('MY_UUIDs', myUUID);
+        });
+}
+
 export default class BroadcastingServices {
     static start() {
         instanceCount += 1;
+
+        BackgroundTimer.runBackgroundTimer(() => { 
+            UUIDGenerator.getRandomUUID((uuid) => {
+                console.log("New UUID: ", uuid);
+                currentUUID = uuid;
+                saveMyUUID(uuid);
+            });
+        }, 1000 * 60 * 60); // Every hour, change UUID
+
+        BackgroundTimer.runBackgroundTimer(() => { 
+            console.log("Making my self discoverable: ", uuid);
+            makeMyselfDiscoverable();
+        }, 1000 * 60 * 3); // Every 3 minutes: make myself discoverable. 
 
         PushNotification.configure({
             // (required) Called when a remote or local notification is opened or received
@@ -73,6 +124,10 @@ export default class BroadcastingServices {
         }, true);           
 
         BroadcastingServices.scan();
+    }
+
+    static makeMyselfDiscoverable() {
+        console.log("Making my self discoverable: ", uuid);
     }
 
     static scan() {
@@ -119,5 +174,6 @@ export default class BroadcastingServices {
         console.log("Stopping Bluetooth");
 
         bleManager.destroy();
+        BackgroundTimer.stopBackgroundTimer();
     }
 }
